1f970e1bf22f4660de1cbd78cd6fdb051c7c08c6
ramki <ramakrishnamanivelthi@gmail.com>
2026-03-21 16:13:39 +0530

    Added experiments 6-11

---
+++ skill-06/src/main/java/com/klu/Skill06Application.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skill06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Skill06Application.class, args);
+	}
+
+}
